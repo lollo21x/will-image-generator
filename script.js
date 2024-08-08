58992f788e0366d0d1a29346a6bfc47551d2111e
lollo21 <90831932+lollo21x@users.noreply.github.com>
2024-08-08 15:28:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function query(data) {
   const response = await fetch(
     "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
     {
-      headers: { Authorization: "Bearer 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" },
+      headers: { Authorization: "Bearer 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" },
       method: "POST",
       body: JSON.stringify(data),
     }
